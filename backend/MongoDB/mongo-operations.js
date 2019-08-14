82c24137caa9e3d81f9b52ad19032092060fb892
jordan <jordan@triple2digital.com>
2019-08-14 15:24:00 +0530

    Testing upsert in mongodb for appending filenames

--- backend/MongoDB/mongo-operations.js
+++ backend/MongoDB/mongo-operations.js
@@ -4,10 +4,24 @@ var url = "mongodb://localhost:27017/";
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   var dbo = db.db("ma-app");
-  var myobj = { name : "tomato", quantity: "5 GMS", processing: "PUREE", file :"./filesystem/Recipes/ChickenBiryani.json"};
-  dbo.collection("variants-configuration").insertOne(myobj, function(err, res) {
-    if (err) throw err;
-    console.log("1 document inserted");
-    db.close();
-  });
+  var myobj = { name : "tomato", quantity: "5 GMS", processing: "PUREE", file :["./filesystem/Recipes/VegBiryani.json"]};
+  // dbo.collection("variants-configuration").insertOne(myobj, function(err, res) {
+  //   if (err) throw err;
+  //   console.log("1 document inserted");
+  //   db.close();
+  // });
+  dbo.collection("variants-configuration").update(
+    {name : myobj.name, quantity: myobj.quantity, processing: myobj.processing},
+    {$push: {file: myobj.file}},
+    {upsert: true, safe: false},
+    function(err,data){
+        if (err){
+            console.log(err);
+        }else{
+            console.log("file added");
+        }
+    }
+);
 });
+
+// { $push: { <field1>: <value1>, ... } }
