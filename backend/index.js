0872209fe49e65896822ffa65387175a67a14264
jordan <jordan@triple2digital.com>
2019-08-14 16:33:50 +0530

    added query and resolver to get the variant details from mongodn

--- backend/index.js
+++ backend/index.js
@@ -2,34 +2,53 @@ let {
   GraphQLServer
 } = require('graphql-yoga');
 
+let mongodb = require('./MongoDB/mongo-operations');
+
 // Type Definitions => Application Schema
 const typeDefs = `
     type Query {
         hello: String!
-    } 
+        ingredientVariants: [variant!]!
+    }
+    
+    type variant {
+      name: String!
+      processing : String!
+      quantity : String!
+      file : [String!]!
+    }
 `
 // Scalar Types -> String, Boolean, Int, Float, ID --> unique identifier similar to String with differences
 // Custom Types ->
 // Resolvers => set of functions
 
 const resolvers = {
-    Query: {
-        hello :(parent, args, ctz, info) => {
-          return "Hello World";
-        },
+  Query: {
+    hello: (parent, args, ctx, info) => {
+      return "Hello World";
     },
+
+    ingredientVariants: (parents, args, ctx, info) => {
+      return mongodb.showAllVariants().then(data => {
+        console.log(data);
+        return(data)
+      })
+    }
+  },
 
 }
 
 const server = new GraphQLServer({
-    typeDefs,
-    resolvers
+  typeDefs,
+  resolvers
 })
 
 const options = {
-  port : 5000,
+  port: 5000,
 }
 
-server.start(options,({ port }) => {
-    console.log("The server is up")
+server.start(options, ({
+  port
+}) => {
+  console.log("The server is up")
 })
